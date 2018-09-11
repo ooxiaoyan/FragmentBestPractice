71d3452129fefc4a1bf61f515c62d7c7b734b69e
ooxiaoyan <810978527@qq.com>
2018-09-11 18:07:16 +0800

    Filling test data into RecyclerView

--- app/src/main/java/com/example/fragmentbestpractice/NewsTitleFragment.java
+++ app/src/main/java/com/example/fragmentbestpractice/NewsTitleFragment.java
@@ -4,13 +4,16 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class NewsTitleFragment extends Fragment {
 
@@ -20,6 +23,11 @@ public class NewsTitleFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.news_title_frag, container, false);
+        RecyclerView newsTitleRecyclerView = view.findViewById(R.id.news_title_recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        newsTitleRecyclerView.setLayoutManager(layoutManager);
+        NewsAdapter adapter = new NewsAdapter(getNews());
+        newsTitleRecyclerView.setAdapter(adapter);
         return view;
     }
 
@@ -31,6 +39,27 @@ public class NewsTitleFragment extends Fragment {
         } else {
             isTwoPane = false;//找不到news_content_layout布局时，为单页模式
         }
+    }
+
+    public List<News> getNews() {
+        List<News> newsList = new ArrayList<>();
+        for (int i = 0; i <= 50; i++) {
+            News news = new News();
+            news.setTitle("This is news title " + i);
+            news.setContent(getRandomLengthContent("This is news content " + i + "."));
+            newsList.add(news);
+        }
+        return newsList;
+    }
+
+    private String getRandomLengthContent(String content) {
+        Random random = new Random();
+        int length = random.nextInt(20) + 1;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            builder.append(content);
+        }
+        return builder.toString();
     }
 
     class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
