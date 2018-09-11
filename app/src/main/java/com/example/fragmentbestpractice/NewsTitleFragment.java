2f5f419f1bca715b31a40540997ac08aa8c37f31
ooxiaoyan <810978527@qq.com>
2018-09-11 17:30:23 +0800

    Add NewsTitleFragment class to display news list

---
+++ app/src/main/java/com/example/fragmentbestpractice/NewsTitleFragment.java
@@ -0,0 +1,31 @@
+package com.example.fragmentbestpractice;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NewsTitleFragment extends Fragment {
+
+    private boolean isTwoPane;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.news_title_frag, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (getActivity().findViewById(R.id.news_content_layout) != null) {
+            isTwoPane = true;//能找到news_content_layout布局时，为双页模式
+        } else {
+            isTwoPane = false;//找不到news_content_layout布局时，为单页模式
+        }
+    }
+}
