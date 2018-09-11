9ca7438cad2ddea60e51702d3ed100e144f9d303
ooxiaoyan <810978527@qq.com>
2018-09-11 15:56:09 +0800

    Add NewsContentActivity class and news_content layout

---
+++ app/src/main/java/com/example/fragmentbestpractice/NewsContentActivity.java
@@ -0,0 +1,27 @@
+package com.example.fragmentbestpractice;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsContentActivity extends AppCompatActivity {
+
+    public void actionStart(Context context, String newsTitle, String newsContent) {
+        Intent intent = new Intent(context, NewsContentActivity.class);
+        intent.putExtra("news title", newsTitle);
+        intent.putExtra("news content", newsContent);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.news_content);
+
+        String newsTitle = getIntent().getStringExtra("news title");//获取传入的新闻标题
+        String newsContent = getIntent().getStringExtra("news content");//获取传入的新闻内容
+        NewsContentFragment newsContentFragment = (NewsContentFragment) getSupportFragmentManager().findFragmentById(R.id.news_content_fragment);
+        newsContentFragment.refresh(newsTitle, newsContent);//刷新NewsContentFragment界面
+    }
+}
