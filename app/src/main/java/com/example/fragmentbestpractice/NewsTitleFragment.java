e64d223e5a951964d514fb069e136e1193077503
ooxiaoyan <810978527@qq.com>
2018-09-11 17:55:07 +0800

    Add an internal class NewsAdapter as an adapter for RecyclerView

--- app/src/main/java/com/example/fragmentbestpractice/NewsTitleFragment.java
+++ app/src/main/java/com/example/fragmentbestpractice/NewsTitleFragment.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class NewsTitleFragment extends Fragment {
 
@@ -28,4 +32,59 @@ public class NewsTitleFragment extends Fragment {
             isTwoPane = false;//找不到news_content_layout布局时，为单页模式
         }
     }
+
+    class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
+
+        private List<News> mNewsList;
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+
+            TextView newsTitleText;
+
+            public ViewHolder(@NonNull View view) {
+                super(view);
+                newsTitleText = view.findViewById(R.id.news_title);
+            }
+        }
+
+        public NewsAdapter(List<News> mNewsList) {
+            this.mNewsList = mNewsList;
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int position) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.news_item, parent, false);
+            final ViewHolder holder = new ViewHolder(view);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    News news = mNewsList.get(holder.getAdapterPosition());
+                    if (isTwoPane) {
+                        //如果是双页模式，则刷新NewsContentFragment中的内容
+                        NewsContentFragment newsContentFragment = (NewsContentFragment) getFragmentManager()
+                                .findFragmentById(R.id.news_content_fragment);
+                        newsContentFragment.refresh(news.getTitle(), news.getContent());
+                    } else {
+                        //如果是单页模式，则直接启动NewsContentActivity
+                        NewsContentActivity.actionStart(getActivity(), news.getTitle(), news.getContent());
+                    }
+                }
+            });
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            News news = mNewsList.get(position);
+            holder.newsTitleText.setText(news.getTitle());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mNewsList.size();
+        }
+    }
+
 }
